d6c9195b1e568423c59ccdd3cb9c269534c4ae59
larry <larrykingsten1@gmail.com>
2023-10-17 08:56:21 +0200

    updated routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -17,7 +17,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/:id", fetchUser);
 router.get("/", fetchAllUsers);
-router.get("/search/:search_key", searchUsers);
+router.post("/search/:search_key", searchUsers);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 // Updating an cubical
